eff94875ce024699a8bb6d912a4e5befbd43ab17
Raveen K Sharma <raveen08@gmail.com>
2020-05-23 01:15:47 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,16 +1,6 @@
-class Person
-  attr_accessor :partner, :name
 
-  def initialize(name)
-    @name = name
-  end
 
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
 
-end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
